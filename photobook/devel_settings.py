583d23a9572f08b7b08933aaa0846fc50981bf8c
Fenykepy <phiroom@lavilotte-rolle.fr>
2019-08-16 14:53:25 +0200

    Add cors headers.

--- photobook/devel_settings.py
+++ photobook/devel_settings.py
@@ -27,8 +27,8 @@ MANAGERS = ADMINS
 # Allow CORS for developpment
 CORS_ORIGIN_ALLOW_ALL = True
 CORS_ORIGIN_WHITELIST = (
-    'localhost:3000',
-    '127.0.0.1:3000'
+    'http://localhost:3000',
+    'http://127.0.0.1:3000'
 )
 CORS_ALLOW_CREDENTIALS = True
 
